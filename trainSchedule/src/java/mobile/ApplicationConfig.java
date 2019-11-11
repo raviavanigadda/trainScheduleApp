6843a1d24289df5df5fca67fffd83f4d522bd04e
ravigamer95 <ravi.gamer95@gmail.com>
2019-11-11 18:08:06 -0500

    Added a sample webservice

--- trainSchedule/src/java/mobile/ApplicationConfig.java
+++ trainSchedule/src/java/mobile/ApplicationConfig.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.Application;
 
 /**
  *
- * @author Ravilion
+ * @author Raviteja
  */
 @javax.ws.rs.ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
--- trainSchedule/src/java/mobile/MainResource.java
+++ trainSchedule/src/java/mobile/MainResource.java
@@ -5,14 +5,23 @@
  */
 package mobile;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 /**
  * REST Web Service
@@ -22,13 +31,27 @@ import javax.ws.rs.core.MediaType;
 @Path("main")
 public class MainResource {
 
+    Connection con= null;
+    Statement stm= null;
+    PreparedStatement ps = null;
+    ResultSet rs= null;
+    JSONObject mainObject = new JSONObject();
+    JSONArray mainArray = new JSONArray();
+    
     @Context
     private UriInfo context;
 
     /**
      * Creates a new instance of MainResource
+     * @throws java.sql.SQLException
      */
-    public MainResource() {
+    public MainResource() throws SQLException {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "mad312team11", "anypw");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MainResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -41,13 +64,78 @@ public class MainResource {
         //TODO return proper representation object
         throw new UnsupportedOperationException();
     }
+    
+    @GET
+    @Path("listUsers")
+    @Produces("application/json")
+    public String getText() {
+       
+        mainObject.accumulate("Status", "Error");
+        mainObject.accumulate("Message", "User doesn't exists");        
+       
+        try {
+       
+            stm = con.createStatement();
+            String sql = "select * from users";
+            rs = stm.executeQuery(sql);
 
-    /**
-     * PUT method for updating or creating an instance of MainResource
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+            int id;
+            String fname, lname, email, password, phone;
+            
+
+            while (rs.next()) {
+                mainObject.clear();
+                id = rs.getInt("id");
+                password = rs.getString("password");
+                fname = rs.getString("firstname");
+                lname = rs.getString("lastname");
+                email = rs.getString("email");
+                phone = rs.getString("phonenumber");
+               
+
+                mainObject.accumulate("id", id);
+                mainObject.accumulate("password", password);
+                mainObject.accumulate("fname", fname);
+                mainObject.accumulate("lname", lname);
+                mainObject.accumulate("email", email);
+                mainObject.accumulate("phone", phone);
+              
+                mainArray.add(mainObject);
+               
+                mainObject.clear();
+                
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MainResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally {
+            closeDBConnection(rs, stm, con);
+            }
+        return mainArray.toString();
+    }
+
+   
+     private void closeDBConnection(ResultSet rs, Statement stm, Connection con) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+            }
+        }
+        if (stm != null) {
+            try {
+                stm.close();
+            } catch (SQLException e) {
+                
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                
+            }
+        }
     }
 }
